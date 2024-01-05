baeac1d35c1789ef81a1df1ca0d0348df0835b99
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-01-05 23:41:32 +0530

    Update callBacksFn.js

--- callBacksFn.js
+++ callBacksFn.js
@@ -1,14 +1,24 @@
-function sum(num1,num2,fntocall){
-    let resut = num1 + num2;
-    fntocall(resut);
-}
+// function sum(num1,num2,fntocall){
+//     let resut = num1 + num2;
+//     fntocall(resut);
+// }
 
-function displayResult(data){
-    console.log("Result of sum is: " + data);
-}
+// function displayResult(data){
+//     console.log("Result of sum is: " + data);
+// }
 
-function displayResultPassive(data){
-    console.log("sum of result is : " + data);
-}
+// function displayResultPassive(data){
+//     console.log("sum of result is : " + data);
+// }
 
-const ans = sum(1,2, displayResultPassive);
+// const ans = sum(1,2, displayResultPassive);
+
+function calArth(a,b,arthFinalFn){
+    const ans = arthFinalFn(a,b)
+    return ans;
+}
+function sum(a,b){
+    return a+b;
+}
+const value = calArth(5,5,sum);
+console.log(value);
