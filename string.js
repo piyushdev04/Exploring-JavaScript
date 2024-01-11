36d1dd6387ffffa12d98f7fdf2b5a176d026a45a
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-01-11 22:31:37 +0530

    Update string.js

--- string.js
+++ string.js
@@ -22,9 +22,51 @@
 // }
 // getSlice("Hello World",0,5);
 
-const value= "Piyush Bhai";
-let ans = value.substr(2,5)
-let ans2 = value.slice(2,5)
+// const value= "Piyush Bhai";
+// let ans = value.substr(2,5)
+// let ans2 = value.slice(2,5)
 
-console.log(ans)
-console.log(ans2)
+// console.log(ans)
+// console.log(ans2)
+
+// function cutIt(str, startIndex, endIndex){
+//     let newStr = "";
+//     for (let i = 0; i < str.length; i++){
+//         if (i >= startIndex && i < endIndex){
+//             newStr = newStr + str[i];
+//         }
+//     }
+//     return newStr;
+// }
+
+// const value = "Piyush bafna";
+
+
+// console.log(cutIt(value, 2, 5));
+
+// Replace: [.replace]
+
+// const str = "Piyush Bhai";
+// console.log(str.replace("Bhai" , "Kool"))
+
+//Split: [.split]
+
+// const value = "Hi my name is Piyush"
+// const words = value.split(" ");
+
+// console.log(words);
+
+// const value = "   Piyush Bafna    "
+
+// console.log(value.trim())
+
+//Upper Case: [.toUpperCase]
+
+// const value = "Piyush Bafna"
+
+// console.log(value.toUpperCase())
+
+//Lower Case: [.toLowerCase]
+const value = "PIYUSH BAFNA"
+
+console.log(value.toLowerCase())
