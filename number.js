f9b9563c37582632e800d7fbf137c1b38ff7cb85
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-01-11 23:47:10 +0530

    String

---
+++ number.js
@@ -0,0 +1,7 @@
+console.log(parseInt("42"));
+console.log(parseInt("42paaaax"));
+console.log(parseInt("3.14"));
+
+console.log(parseFloat("42"));
+console.log(parseFloat("42paaaax"));
+console.log(parseFloat("3.14"));
--- string.js
+++ string.js
@@ -1,72 +1,72 @@
 //getLength: [.length]
 
-// function getLength(str) {
-//     console.log("Original String:", str);
-//     console.log("Length:", str.length);
-// }
-// getLength("Ram Ram Bhai Sareyane!!!")
+function getLength(str) {
+    console.log("Original String:", str);
+    console.log("Length:", str.length);
+}
+getLength("Ram Ram Bhai Sareyane!!!")
 
 //findIndex: [.indexOf] , [lastIndexOf]
 
-// function findIndexOf(str, target){
-//     console.log("Original String:", str);
-//     console.log("Index:", str.lastIndexOf(target));
-// }
-// findIndexOf("RAM RAM BHAI","RAM");
+function findIndexOf(str, target){
+    console.log("Original String:", str);
+    console.log("Index:", str.lastIndexOf(target));
+}
+findIndexOf("RAM RAM BHAI","RAM");
 
 //getSlice: [.slice]
 
-// function getSlice(str,start,end){
-//     console.log("Original String:",str);
-//     console.log("After slice:", str.slice(start,end));
-// }
-// getSlice("Hello World",0,5);
+function getSlice(str,start,end){
+    console.log("Original String:",str);
+    console.log("After slice:", str.slice(start,end));
+}
+getSlice("Hello World",0,5);
 
-// const value= "Piyush Bhai";
-// let ans = value.substr(2,5)
-// let ans2 = value.slice(2,5)
+const value= "Piyush Bhai";
+let ans = value.substr(2,5)
+let ans2 = value.slice(2,5)
 
-// console.log(ans)
-// console.log(ans2)
+console.log(ans)
+console.log(ans2)
 
-// function cutIt(str, startIndex, endIndex){
-//     let newStr = "";
-//     for (let i = 0; i < str.length; i++){
-//         if (i >= startIndex && i < endIndex){
-//             newStr = newStr + str[i];
-//         }
-//     }
-//     return newStr;
-// }
+function cutIt(str, startIndex, endIndex){
+    let newStr = "";
+    for (let i = 0; i < str.length; i++){
+        if (i >= startIndex && i < endIndex){
+            newStr = newStr + str[i];
+        }
+    }
+    return newStr;
+}
 
-// const value = "Piyush bafna";
+const value1 = "Piyush bafna";
 
 
-// console.log(cutIt(value, 2, 5));
+console.log(cutIt(value1, 2, 5));
 
 // Replace: [.replace]
 
-// const str = "Piyush Bhai";
-// console.log(str.replace("Bhai" , "Kool"))
+const str = "Piyush Bhai";
+console.log(str.replace("Bhai" , "Kool"))
 
 //Split: [.split]
 
-// const value = "Hi my name is Piyush"
-// const words = value.split(" ");
+const value2 = "Hi my name is Piyush"
+const words = value2.split(" ");
 
 // console.log(words);
 
-// const value = "   Piyush Bafna    "
+const value3 = "   Piyush Bafna    "
 
-// console.log(value.trim())
+console.log(value3.trim())
 
 //Upper Case: [.toUpperCase]
 
-// const value = "Piyush Bafna"
+const value4 = "Piyush Bafna"
 
-// console.log(value.toUpperCase())
+console.log(value4.toUpperCase())
 
 //Lower Case: [.toLowerCase]
-const value = "PIYUSH BAFNA"
+const value5 = "PIYUSH BAFNA"
 
-console.log(value.toLowerCase())
+console.log(value5.toLowerCase())
