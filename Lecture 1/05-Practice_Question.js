108e782a0d3cf75295f95ca9d29a9a45400ec2e1
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-19 22:09:39 +0530

    Create 05-Practice_Question.js

---
+++ Lecture 1/05-Practice_Question.js
@@ -0,0 +1,21 @@
+// Qs1. Create a const object called "product" to store information shown in the picture.
+
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+
+console.log(product);
+
+// Qs2. Create a const object called "profile" to store information shown in the picture.
+
+const profile = {
+    username: "@batman",
+    isFollow: false,
+    followers: 123,
+    following: 123
+};
+
+console.log(profile);
