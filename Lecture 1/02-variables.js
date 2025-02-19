48cf464bd5093e652a258faab0fa694c9f8696e3
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-19 19:51:20 +0530

    Create 02-variables.js

---
+++ Lecture 1/02-variables.js
@@ -0,0 +1,25 @@
+/*
+    Variables
+    - Variables are containers for data.
+*/
+
+fullName = "Batman!";
+age = 21;
+price = 99.99;
+x = null;
+y = undefined;
+// boolean
+isFollow = true;
+notFollow = false;
+console.log(fullName + " " + age + " " + price + " " + x + " " + y + " "+ isFollow + " " + notFollow);
+
+
+/*---------------------------------------------------*/
+
+/*
+    Variable Rules
+    - Variable names are case sensitive; "a" & "A" is different.
+    - Only letters, digits, underscore(_) and $ is allowed. (not even space)
+    - Only a letter, underscore(_) or $ should be 1st character.
+    - Reserved words cannot be variable names.
+*/
