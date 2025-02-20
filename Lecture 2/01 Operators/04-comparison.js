5f3ad4672b8195f8d9b0a8b3a9f29d641b356629
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-20 10:49:40 +0530

    Create 04-comparison.js

---
+++ Lecture 2/01 Operators/04-comparison.js
@@ -0,0 +1,15 @@
+// Comparison Operator
+let a = 5;
+let b = 5; 
+
+console.log("5 == 2", a == b); // even if we have string ex: "5" it will convert string -> number and check
+console.log("5 != 2", a != b);
+
+console.log("a === b", a === b); // for b = "5" it'll return false bcuz "===" it dosen't change type also
+console.log("a !== b", a !== b);
+
+console.log("5 > 3", a > b);
+console.log("5 < 3", a < b);
+
+console.log("5 <= 5", a <= b); // for b = 5
+console.log("5 >= 5", a >= b);
