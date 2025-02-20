4a194b0ac41b34b88ad86b12e9a6e3d19b81d4b8
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-20 12:11:38 +0530

    Create 06-ternary.js

---
+++ Lecture 2/01 Operators/06-ternary.js
@@ -0,0 +1,10 @@
+/*
+    Ternary Operators: operator which works on 3 operand
+    
+    condition ? true output : false output
+*/
+
+let age = 16;
+
+let result = age >= 18 ? "adult" : "not adult";
+console.log(result);
