9d7332e7f4402407dc6cb6b4c457b030b6e01d4a
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-20 10:49:37 +0530

    Create 03-assignment.js

---
+++ Lecture 2/01 Operators/03-assignment.js
@@ -0,0 +1,10 @@
+// Assignment Operators
+let a = 5;
+let b = 2;
+
+a += 4; // a = a + 4
+a -= 4; // a = a - 4
+a *= 4; // a = a * 4
+a /= 4; // a = a / 4
+a %= 4; // a = a % 4
+a **= 4; // a = a ** 4
