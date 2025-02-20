5236d96e90746930a2d59f43498d7d62e3f1ecc5
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-21 00:30:15 +0530

    Create 01-string.js

---
+++ Lecture 3/02-Strings/01-string.js
@@ -0,0 +1,8 @@
+// Strings: String is a sequence of characters used to represent text
+
+let str = "BATMAN!"; // created string
+console.log(str); // printed string in console
+
+console.log(str.length); // String Lenght
+
+console.log(str[0], str[2]); // String Indices
