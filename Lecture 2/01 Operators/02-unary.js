f058f0ed407d3ea60cf7e85ee98d92c9af7b5f3a
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-20 10:49:32 +0530

    Create 02-unary.js

---
+++ Lecture 2/01 Operators/02-unary.js
@@ -0,0 +1,29 @@
+// Unary Operators
+
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, " & b = ", b);
+
+// Increment
+b++; // b + 1 [Post]
+console.log("b = ", b);
+
+console.log("++b", ++b); // [pre]
+/*
+    Post and Pre Increment
+    Post: a++
+    Pre: ++a
+*/
+
+
+// Decrement
+a--; // a - 1 [Post]
+console.log("a = ", a);
+
+console.log("--a", --a);
+/*
+    Post and Pre Decrement
+    Post: a--
+    Pre: --a
+*/
