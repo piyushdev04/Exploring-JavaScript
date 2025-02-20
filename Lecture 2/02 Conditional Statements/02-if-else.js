01e6f3afb2e829229fbb0333818436b8b1c065a2
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-20 11:58:19 +0530

    Create 02-if-else.js

---
+++ Lecture 2/02 Conditional Statements/02-if-else.js
@@ -0,0 +1,34 @@
+// If-else statement
+
+// Ex1
+let mode = "light";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+} else {
+    color = "white";
+}
+
+console.log(color);
+
+
+// Ex2
+
+let age = 25;
+
+if (age >= 18) {
+    console.log("vote");
+} else {
+    console.log("not vote");
+}
+
+// odd or even
+
+let num = 7;
+
+if (num % 2 === 0) {
+    console.log(num, "is even");
+} else {
+    console.log(num, "is odd");
+}
