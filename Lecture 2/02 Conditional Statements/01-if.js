d9e0794f07413fa257bfbc32d15158f82fa23934
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-20 11:58:15 +0530

    Create 01-if.js

---
+++ Lecture 2/02 Conditional Statements/01-if.js
@@ -0,0 +1,26 @@
+// If Statement
+
+// Ex1
+let age = 15;
+
+if (age >= 18) {
+    console.log("you can vote");
+}
+
+if (age < 18) {
+    console.log("you CANNOT vote");
+}
+
+// Ex2. light/dark mode
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if (mode === "light") {
+    color = "white";
+}
+
+console.log(color);
