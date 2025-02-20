5b2877da9f7aec0cf6b25a1c659dc00b6f0e023b
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-21 00:30:05 +0530

    Create 01-forLoop.js

---
+++ Lecture 3/01-Loops/01-forLoop.js
@@ -0,0 +1,20 @@
+// for Loop
+
+// Print 1 to 5
+for (let i = 1; i <= 5; i++) {
+    console.log("Piyush");
+}
+
+console.log("loop has ended");
+
+// print i
+for (let i = 1; i <= 5; i++) {
+    console.log("i = ", i);
+}
+
+// Calculate sum of 1 to 5
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+}
+console.log("sum = ", sum);
