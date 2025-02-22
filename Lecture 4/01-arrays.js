7846c201f3a2f6ae12ca9fb40230259d4007fed6
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-22 23:52:57 +0530

    Create 01-arrays.js

---
+++ Lecture 4/01-arrays.js
@@ -0,0 +1,11 @@
+/*
+    Arrays: Collections of items.
+    Array -> mutable
+*/
+
+let marks = [96, 75, 48, 83, 66];
+console.log(marks);
+console.log(marks.length); //property
+
+let heroes = ["ironman", "batman", "hulk", "thor", "shaktiman", "spiderman", "antman"];
+console.log(heroes);
