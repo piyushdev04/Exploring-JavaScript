8f9271797f3ceba36e8b16abd215fff99d3d3349
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-22 23:53:03 +0530

    Create 02-indices.js

---
+++ Lecture 4/02-indices.js
@@ -0,0 +1,15 @@
+/*
+    Array Indices:
+    arr[0], arr[1], arr[2]......
+*/
+
+let marks = [97, 56, 48, 63, 99, 87];
+
+console.log(marks); // [97, 56, 48, 63, 99, 87]
+
+console.log(marks[1]); // 56
+
+marks[3] = 77;
+console.log(marks[3]); // 77
+
+console.log(marks); // [97, 56, 48, 77, 99, 87]
